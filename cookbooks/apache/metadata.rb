f6541c781f138c5aceb673b88d27dba422b19c68
ec2-user <rajeshwari.makond@sap.com>
2017-06-25 06:54:36 -0400

    adding apache cookbook

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'apache'
+maintainer       'REANCLOUD'
+maintainer_email 'Rajeshwari'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
